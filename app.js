8b31c137fdfde52559bcdfafd0219f487fc9d59f
mertcosann <134630975+mertcosann@users.noreply.github.com>
2024-04-17 12:21:05 +0300

    taskagıtmakas

--- app.js
+++ app.js
@@ -1,200 +1,59 @@
-let board = [];
-let rows = 8;
-let columns = 8;
+const computerChoiseDisplay=document.getElementById('computer-choise')
+const userChoiseDisplay=document.getElementById('user-choise')
+const resultDisplay=document.getElementById('result')
 
-let minesCount = 11; //mayın sayısı
-let minesLocation = []; // "2-2", "3-4", "2-1"
+const possibleChoices = document.querySelectorAll('button')
 
-let tilesClicked = 0;
-let flagEnabled = false;
+let userChoice
+let computerChoice
+let result
 
-let gameOver = false;
+possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener("click", (e)=>{
+    userChoice=e.target.id
+    userChoiseDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
 
-window.onload = function() 
+function generateComputerChoice()
 {
-    startGame();
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length+1)
+    if(randomNumber === 1) {computerChoice = "tas"}
+    if(randomNumber === 2){ computerChoice = "kagit"}
+    if(randomNumber === 3) { computerChoice = "makas "}
+    computerChoiseDisplay.innerHTML = computerChoice
 }
 
-function setMines() 
+function getResult()
 {
-    let minesLeft = minesCount;
-    while (minesLeft > 0) 
-    { 
-        let r = Math.floor(Math.random() * rows);
-        let c = Math.floor(Math.random() * columns);
-        let id = r.toString() + "-" + c.toString();
-
-        if (!minesLocation.includes(id)) 
-        {
-            minesLocation.push(id);
-            minesLeft -= 1;
-        }
-    }
-}
-
-
-function startGame() 
-{
-    document.getElementById("mines-count").innerText = minesCount;
-    document.getElementById("flag-button").addEventListener("click", setFlag);
-    setMines();
-
-    //populate our board
-    for (let r = 0; r < rows; r++) 
+    if(computerChoice === userChoice)
     {
-        let row = [];
-        for (let c = 0; c < columns; c++) 
-        {
-            //<div id="0-0"></div>
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.addEventListener("click", clickTile);
-            document.getElementById("board").append(tile);
-            row.push(tile);
-        }
-        board.push(row);
+        result="Berabere!"
     }
-
-    console.log(board);
-}
-
-function setFlag() 
-{
-    if (flagEnabled) 
+    if(computerChoice === "tas" && userChoice === "makas")
     {
-        flagEnabled = false;
-        document.getElementById("flag-button").style.backgroundColor = "lightgray";
+        result="Maalesef...Kaybettiniz!"
     }
-    else 
+    if(computerChoice === "tas" && userChoice === "kagit")
     {
-        flagEnabled = true;
-        document.getElementById("flag-button").style.backgroundColor = "darkgray";
+        result="Tebrikler...Kazandınız!"
     }
-}
-
-function clickTile()
-{
-    if (gameOver || this.classList.contains("tile-clicked")) 
+    if(computerChoice === "kagit" && userChoice === "makas")
     {
-        return;
+        result="Tebrikler...Kazandınız!"
     }
-
-    let tile = this;
-    if (flagEnabled) 
+    if(computerChoice === "kagit" && userChoice === "tas")
     {
-        if (tile.innerText == "") 
-        {
-            tile.innerText = "🚩";
-        }
-        else if (tile.innerText == "🚩") 
-        {
-            tile.innerText = "";
-        }
-        return;
+        result="Maalesef...Kaybettiniz!"
     }
-
-    if (minesLocation.includes(tile.id)) 
+    if(computerChoice === "makas" && userChoice === "kagit")
     {
-        // alert("GAME OVER");
-        gameOver = true;
-        revealMines();
-        return;
+        result="Maalesef...Kaybettiniz!"
     }
-
-
-    let coords = tile.id.split("-"); // "0-0" -> ["0", "0"]
-    let r = parseInt(coords[0]);
-    let c = parseInt(coords[1]);
-    checkMine(r, c);
-
-}
-
-function revealMines() 
-{
-    for (let r= 0; r < rows; r++) 
+    if(computerChoice === "makas" && userChoice === "tas")
     {
-        for (let c = 0; c < columns; c++) 
-        {
-            let tile = board[r][c];
-            if (minesLocation.includes(tile.id)) 
-            {
-                tile.innerText = "💣";
-                tile.style.backgroundColor = "red";                
-            }
-        }
+        result="Tebrikler...Kazandınız!"
     }
-}
-
-function checkMine(r, c) 
-{
-    if (r < 0 || r >= rows || c < 0 || c >= columns) 
-    {
-        return;
-    }
-    if (board[r][c].classList.contains("tile-clicked")) 
-    {
-        return;
-    }
-
-    board[r][c].classList.add("tile-clicked");
-    tilesClicked += 1;
-
-    let minesFound = 0;
-
-    //top 3
-    minesFound += checkTile(r-1, c-1);      //top left
-    minesFound += checkTile(r-1, c);        //top 
-    minesFound += checkTile(r-1, c+1);      //top right
-
-    //left and right
-    minesFound += checkTile(r, c-1);        //left
-    minesFound += checkTile(r, c+1);        //right
-
-    //bottom 3
-    minesFound += checkTile(r+1, c-1);      //bottom left
-    minesFound += checkTile(r+1, c);        //bottom 
-    minesFound += checkTile(r+1, c+1);      //bottom right
-
-    if (minesFound > 0) 
-    {
-        board[r][c].innerText = minesFound;
-        board[r][c].classList.add("x" + minesFound.toString());
-    }
-    else 
-    {
-        board[r][c].innerText = "";
-        
-        //top 3
-        checkMine(r-1, c-1);    //top left
-        checkMine(r-1, c);      //top
-        checkMine(r-1, c+1);    //top right
-
-        //left and right
-        checkMine(r, c-1);      //left
-        checkMine(r, c+1);      //right
-
-        //bottom 3
-        checkMine(r+1, c-1);    //bottom left
-        checkMine(r+1, c);      //bottom
-        checkMine(r+1, c+1);    //bottom right
-    }
-
-    if (tilesClicked == rows * columns - minesCount) 
-    {
-        document.getElementById("mines-count").innerText = "Cleared";
-        gameOver = true;
-    }
-}
-
-function checkTile(r, c) 
-{
-    if (r < 0 || r >= rows || c < 0 || c >= columns) 
-    {
-        return 0;
-    }
-    if (minesLocation.includes(r.toString() + "-" + c.toString())) 
-    {
-        return 1;
-    }
-    return 0;
+    
+    resultDisplay.innerHTML = result
 }
